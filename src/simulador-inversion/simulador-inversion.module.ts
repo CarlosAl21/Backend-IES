04e0adc6964af9147df66514d1dc1cd049fa6103
CarlosAl21 <g4to101@hotmail.com>
2025-10-10 17:08:25 -0500

    cambio menor

--- src/simulador-inversion/simulador-inversion.module.ts
+++ src/simulador-inversion/simulador-inversion.module.ts
@@ -3,10 +3,11 @@ import { SimuladorInversionService } from './simulador-inversion.service';
 import { SimuladorInversionController } from './simulador-inversion.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SimuladorInversion } from './entities/simulador-inversion.entity';
+import { TipoInversion } from 'src/tipo-inversion/entities/tipo-inversion.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([SimuladorInversion]),
+    TypeOrmModule.forFeature([SimuladorInversion, TipoInversion]),
   ],
   controllers: [SimuladorInversionController],
   providers: [SimuladorInversionService],

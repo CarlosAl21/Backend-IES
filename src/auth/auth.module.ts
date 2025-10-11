a371b734b0966deb3e9ca4b3da645b4402f2e64f
CarlosAl21 <g4to101@hotmail.com>
2025-10-11 12:26:25 -0500

    correccion de error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,27 +1,31 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
-import { UserModule } from 'src/user/user.module'; // <- Importar UserModule
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
-    UserModule, // <- Esto permite usar UserService
+    UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([User]),
     ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET', 'secretKey'), 
-        signOptions: { expiresIn: '6h' }, 
+        secret: configService.get<string>('JWT_SECRET', 'secretKey'),
+        signOptions: { expiresIn: '6h' },
       }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [PassportModule, JwtModule, AuthService],
 })
 export class AuthModule {}
